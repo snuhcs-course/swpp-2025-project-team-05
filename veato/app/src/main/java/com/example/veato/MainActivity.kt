2b2ce7ddb59a9640418787e7fa043a6d9b496e68
ahchow <chow170222@snu.ac.kr>
2025-10-16 16:56:22 +0900

    solved logout problem

--- veato/app/src/main/java/com/example/veato/MainActivity.kt
+++ veato/app/src/main/java/com/example/veato/MainActivity.kt
@@ -28,6 +28,10 @@ class MainActivity : AppCompatActivity() {
 
         btnLogout.setOnClickListener {
             auth.signOut()
+
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
             finish()
         }
 
